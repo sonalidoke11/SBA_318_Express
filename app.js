0b3388ff130bc0638d79c0510cba7aec6b111840
sonalidoke11 <sonalidoke810@gmail.com>
2024-10-17 14:44:46 -0400

    changes for pug

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import path from 'path';
-import bodyParser from 'body-parser';
+//import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+//const PORT = 4004
 
 // Using Pug as the view engine
 app.set('view engine', 'pug');
@@ -13,14 +13,18 @@ app.set('views', './views');
 
 
 // Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Static files
 app.use(express.static('public'));
 
 // Routes
+app.get('/', (req,res) =>{
+  res.render('layout')
+})
+
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
 
@@ -36,7 +40,7 @@ app.use((err, req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4004;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
